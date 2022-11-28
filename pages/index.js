b3d40792f82a0683ffdb64aba5bbce9b9e890091
annielyl <anniely.lemos@gmail.com>
2022-11-28 09:19:44 -0300

    fazendo ajustes

--- pages/index.js
+++ pages/index.js
@@ -65,49 +65,51 @@ export default function Home() {
   }, [address]);
 
   return (
-    <div className={styles.container}>
-      <input className={styles.search} type='text' placeholder="  Rio Grande" onChange={changeAddressHandler}>
-      </input>
-      <iframe
-        className={styles.map}
-        height='450'
-        loading='lazy'
-        allowFullScreen
-        referrerPolicy='no-referrer-when-downgrade'
-        src={`https://www.google.com/maps/embed/v1/place?key=${
-          config.googleMapsAPIKey
-        }&q=${address || 'Brasil'}`}
-      />
-      <div>
-        { tweets.map(el => {
-            return (
-              <div className={styles.tweet} key={el.id}>
-                <div className={styles.userInfo}>
-                  <img
-                    className={styles.profilePic}
-                    src={el.user.profile_image_url}
-                  />
-                  <span className={styles.userName}>{el.user.name}</span>
-                  <span className={styles.userScreenName}>
-                    @{el.user.screen_name}
-                  </span>
-                </div>
-                <p className={styles.tweetText}>{el.text}</p>
-                <div className={styles.interactions}>
-                  <span className={styles.interaction}>
-                    <AiOutlineRetweet className={styles.icon} />{' '}
-                    {el.retweet_count}
-                  </span>
-                  <span className={styles.interaction}>
-                    <AiFillHeart className={styles.icon} /> {el.favorite_count}
-                  </span>
-                  <p className={styles.date}>{formatTimestampToDateAndTime(el.created_at)}</p>
-                </div>
-              </div>
-            );
-          })
-        }
-      </div>
+    <div className={styles.all}>
+        <div className={styles.container}>
+        <input className={styles.search} type='text' placeholder="  Busque tweets perto de você" onChange={changeAddressHandler}>
+        </input>
+        <iframe
+          className={styles.map}
+          height='450'
+          loading='lazy'
+          allowFullScreen
+          referrerPolicy='no-referrer-when-downgrade'
+          src={`https://www.google.com/maps/embed/v1/place?key=${
+            config.googleMapsAPIKey
+          }&q=${address || 'Brasil'}`}
+        />
+        </div>
+          <div className={styles.container}>
+            { tweets.map(el => {
+                return (
+                  <div className={styles.tweet} key={el.id}>
+                    <div className={styles.userInfo}>
+                      <img
+                        className={styles.profilePic}
+                        src={el.user.profile_image_url}
+                      />
+                      <span className={styles.userName}>{el.user.name}</span>
+                      <span className={styles.userScreenName}>
+                        @{el.user.screen_name}
+                      </span>
+                    </div>
+                    <p className={styles.tweetText}>{el.text}</p>
+                    <div className={styles.interactions}>
+                      <span className={styles.interaction}>
+                        <AiOutlineRetweet className={styles.icon} />{' '}
+                        {el.retweet_count}
+                      </span>
+                      <span className={styles.interaction}>
+                        <AiFillHeart className={styles.icon} /> {el.favorite_count}
+                      </span>
+                      <p className={styles.date}>{formatTimestampToDateAndTime(el.created_at)}</p>
+                    </div>
+                  </div>
+                );
+              })
+            }
+          </div>
     </div>
   );
 }
