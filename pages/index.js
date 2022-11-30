bbb326d1acff5178dc45267a94f5fcc1a94e0734
Laura A. Dalmolin <laura.aguiar.dalmolin@gmail.com>
2022-11-29 22:56:59 -0300

    fixing bug and improving layout

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,11 @@ export default function Home() {
   const [address, setAddress] = useState('Rio+Grande+RS');
   const [tweets, setTweets] = useState([]);
 
-  const debouncedSearch = debounce((value) => {
+  const debouncedSearch = debounce(async (value) => {
+    if (!value) return;
     setAddress(value);
     getGeoLocation(value);
-  }, 1000);
+  }, 300);
 
   //Essa funçao pega a geolocalização dos tweets atraves da rota da api citada abaixo
   //de acordo om o endereço utilizado, são extraidos os dados de latitude e longitude do
@@ -40,7 +41,6 @@ export default function Home() {
       const url = `http://localhost:3000/api/get-tweets?lat=${lat}&long=${long}`;
       const res = await fetch(url);
       const response = await res.json();
-      console.log(response);
       setTweets(response.statuses);
     } catch (err) {
       console.log(err);
@@ -53,59 +53,63 @@ export default function Home() {
 
   useEffect(() => {
     getGeoLocation(address);
-  }, [address]);
+  }, []);
 
   //Nesse trecho do codigo é definido o front-end da pagina
   return (
-    <div className={styles.all}>
+    <div className={styles.container}>
       {/* Nesse trecho é definido a barra de busca, onde sera inserido pelo usuario a cidade desejada */}
-        <div className={styles.container}>
-        <input className={styles.search} type='text' placeholder="  Busque tweets perto de você" onChange={changeAddressHandler}>
-        </input>
+      <div className={styles.column}>
+        <h1 className={styles.title}>Geo Trending 🌍</h1>
+        <input
+          className={styles.search}
+          type='text'
+          placeholder='Busque tweets perto de você'
+          onChange={changeAddressHandler}
+        ></input>
         <iframe
-        // aqui é definido o mapa, deixando como default a regiao do Brasil
+          // aqui é definido o mapa, deixando como default a regiao do Brasil
           className={styles.map}
           height='450'
           loading='lazy'
           allowFullScreen
           referrerPolicy='no-referrer-when-downgrade'
-          src={`https://www.google.com/maps/embed/v1/place?key=${
-            config.googleMapsAPIKey
-          }&q=${address || 'Brasil'}`}
+          src={`https://www.google.com/maps/embed/v1/place?key=${config.googleMapsAPIKey}&q=${address}`}
         />
-        </div>
-          <div className={styles.container}>
-            {/* Nesse trecho é feito o mapeamento e retorno dos 10 ultimos tweets dessa região */}
-            {/* Trazendo o nome du usuario, nome, imagem de perfil, o conteudo da mensagem e a hora */}
-            { tweets.map(el => {
-                return (
-                  <div className={styles.tweet} key={el.id}>
-                    <div className={styles.userInfo}>
-                      <img
-                        className={styles.profilePic}
-                        src={el.user.profile_image_url}
-                      />
-                      <span className={styles.userName}>{el.user.name}</span>
-                      <span className={styles.userScreenName}>
-                        @{el.user.screen_name}
-                      </span>
-                    </div>
-                    <p className={styles.tweetText}>{el.text}</p>
-                    <div className={styles.interactions}>
-                      <span className={styles.interaction}>
-                        <AiOutlineRetweet className={styles.icon} />{' '}
-                        {el.retweet_count}
-                      </span>
-                      <span className={styles.interaction}>
-                        <AiFillHeart className={styles.icon} /> {el.favorite_count}
-                      </span>
-                      <p className={styles.date}>{formatTimestampToDateAndTime(el.created_at)}</p>
-                    </div>
-                  </div>
-                );
-              })
-            }
-          </div>
+      </div>
+      <div className={styles.column}>
+        {/* Nesse trecho é feito o mapeamento e retorno dos 10 ultimos tweets dessa região */}
+        {/* Trazendo o nome du usuario, nome, imagem de perfil, o conteudo da mensagem e a hora */}
+        {tweets.map((el) => {
+          return (
+            <div className={styles.tweet} key={el.id}>
+              <div className={styles.userInfo}>
+                <img
+                  className={styles.profilePic}
+                  src={el.user.profile_image_url}
+                />
+                <span className={styles.userName}>{el.user.name}</span>
+                <span className={styles.userScreenName}>
+                  @{el.user.screen_name}
+                </span>
+              </div>
+              <p className={styles.tweetText}>{el.text}</p>
+              <div className={styles.interactions}>
+                <span className={styles.interaction}>
+                  <AiOutlineRetweet className={styles.icon} />{' '}
+                  {el.retweet_count}
+                </span>
+                <span className={styles.interaction}>
+                  <AiFillHeart className={styles.icon} /> {el.favorite_count}
+                </span>
+                <p className={styles.date}>
+                  {formatTimestampToDateAndTime(el.created_at)}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- utils/strings.js
+++ utils/strings.js
@@ -1,40 +1,45 @@
-//como a data vem da api no formato GMT é preciso fazer a conversão
+// Como a data vem da api em um formato não muito legível
+// Então, é preciso fazer a conversão para algo mais amigável ao usuário
 
-//essa constante faz o mapeamento para converter os meses de nome para numero
+// Esse hashmap mapeia os meses para números
 const charMap = new Map([
-    ['Jan', 1],
-    ['Feb', 2],
-    ['Mar', 3],
-    ['Apr', 4],
-    ['May', 5],
-    ['Jun', 6],
-    ['Jul', 7],
-    ['Aug', 8],
-    ['Sep', 9],
-    ['Oct', 10],
-    ['Nov', 11],
-    ['Dec', 12],
-  ]);
+  ['Jan', 1],
+  ['Feb', 2],
+  ['Mar', 3],
+  ['Apr', 4],
+  ['May', 5],
+  ['Jun', 6],
+  ['Jul', 7],
+  ['Aug', 8],
+  ['Sep', 9],
+  ['Oct', 10],
+  ['Nov', 11],
+  ['Dec', 12],
+]);
   
-  //essa função formata a data, alterando as posições do array para manter o padrao do Brasil dia-mes-ano
-  const getDate = (arr) => {
-    return  arr[arr.length - 1] + '-' + charMap.get(arr[1]) + '-' + arr[2];
-  }
+// Essa função formata a data, alterando as posições do array para manter o padrão ano-mes-dia
+const getDate = (arr) => {
+  return  arr[arr.length - 1] + '-' + charMap.get(arr[1]) + '-' + arr[2];
+}
   
-  //essa função retorna apenas o horario do tweet
-  const getTime = (arr) => {
-    return arr[3];
-  }
+// Essa função retorna apenas o horario do tweet
+const getTime = (arr) => {
+  return arr[3];
+}
 
-//como o nome da função indica, aqui é feita a conversão da data
-//pegando data e tempo das funções e unindo
-// após isso é definido o pais e zona, nesse caso Brasil, Sao Paulo  
+// Como o nome da função indica, aqui é feita a conversão da data
+// Constrói-se um objeto Date a partir de uma timestamp formada pela data e o tempo
+// além da informação de fuso horário, no caso GMT (+00:00)
   const formatTimestampToDateAndTime = (timestamp) => {
     const arr = timestamp.split(' ');
     const dateStr = getDate(arr);
     const time = getTime(arr);
     const date = new Date(`${dateStr}T${time}.000+00:00`);
-  
+    
+    // aqui, utiliza-se uma função nativa do JS para converter a data gerada
+    // para um formato mais legível ao usuário
+    // escolhemos o locale pt-BR e também fazemos a conversão para a timezone
+    // de São Paulo, visto que a API retorna em GMT (Londres)
     return date.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
   }
   
